7c781589ab3459bfb5c0489dc18348434fd38b68
artem <bam10112002@gmail.com>
2025-07-12 11:20:59 +0000

    -

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
-import { LoginProvider } from '@/services/LoginContext.jsx'
+import { LoginProvider } from '@/services/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
