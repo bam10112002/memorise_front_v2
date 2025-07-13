2eea9d2e5e8317c89ef89ea8a9c72fa6c4138b06
artem <bam10112002@gmail.com>
2025-07-13 05:38:27 +0000

    -

--- src/services/conf.js
+++ src/services/conf.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://213.176.65.159.nip.io:4435';
-export const WS_BASE_URL = 'wss://213.176.65.159.nip.io:4435';
+const API_BASE_URL = 'https://213.176.65.159.nip.io';
+export const WS_BASE_URL = 'wss://213.176.65.159.nip.io';
 
 export default API_BASE_URL;
