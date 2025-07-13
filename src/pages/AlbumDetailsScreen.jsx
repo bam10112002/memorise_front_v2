3486dd1f526971113e80b0406c2ed704e6a3747d
artem <bam10112002@gmail.com>
2025-07-13 06:34:01 +0000

    stable

--- src/pages/AlbumDetailsScreen.jsx
+++ src/pages/AlbumDetailsScreen.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import WS_BASE_URL from '@/services/conf';
 import { useLogin } from '@/services/AuthContext';
 
+
 const AlbumDetailsScreen = ({ showNotification }) => {
   const navigate = useNavigate();
   const { id } = useParams();
